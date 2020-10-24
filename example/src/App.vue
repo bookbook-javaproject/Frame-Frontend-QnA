4b82a1dd17363c98d6d96c3927d969131ba44c45
_uchanlee <uchanlee.dev@gmail.com>
2020-10-25 02:43:44 +0900

    [FIX] library

---
+++ example/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <messenger />
+</template>
+
+<script>
+import Messenger from 'frame-qna';
+
+export default {
+  name: 'App',
+  components: { Messenger },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
