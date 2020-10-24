ef9f651a78e623008f26db8d5918752d50073ee5
_uchanlee <uchanlee.dev@gmail.com>
2020-10-24 16:07:49 +0900

    [UPDATE] #19 채팅 sender UI 제작

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,2 +1,3 @@
 export { default as chattingImage } from './chatting_image.svg';
 export { default as closeImage } from './close_image.svg';
+export { default as senderImage } from './sender_image.svg';
