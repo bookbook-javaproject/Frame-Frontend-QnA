034bda8149e8bf6fc2f2e738a3c14956469210be
_uchanlee <uchanlee.dev@gmail.com>
2020-10-24 14:30:52 +0900

    [CREATE] #4 채팅 UI animation 효과 적용

---
+++ src/views/chatting/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div
+    v-bind:class="isOpen ? 'chatting__wrapper isOpen' : 'chatting__wrapper'"
+    v-if="animationRender"
+    @animationend="onAnimationEnd"
+  >
+    <header>QnA</header>
+    <main></main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Chatting',
+  props: {
+    isOpen: Boolean,
+  },
+  watch: {
+    isOpen() {
+      if (this.isOpen) {
+        this.animationRender = true;
+      }
+    },
+  },
+  data() {
+    return {
+      animationRender: this.isOpen,
+    };
+  },
+  methods: {
+    onAnimationEnd() {
+      if (!this.isOpen) {
+        this.animationRender = false;
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import './style';
+</style>
