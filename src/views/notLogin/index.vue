cafa133be820505c4292a56c2cfbe35db67c3671
_uchanlee <uchanlee.dev@gmail.com>
2020-10-24 15:24:54 +0900

    [CREATE] #13 비로그인 상태일때 애니메이션 효과

---
+++ src/views/notLogin/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="not-login__wrapper">
+    <p>로그인 후 이용할 수 있습니다</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotLogin',
+};
+</script>
+
+<style lang="scss" scoped>
+@import './style/index.scss';
+</style>
